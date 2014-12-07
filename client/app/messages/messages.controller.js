86952d4d529034a7c8c16b5eef276a4b1201ab28
EvilDmitri <dmitribrach@gmail.com>
2014-12-07 19:43:47 +0200

    message user name

--- client/app/messages/messages.controller.js
+++ client/app/messages/messages.controller.js
@@ -1,19 +1,21 @@
 'use strict';
 
 angular.module('fullAppApp')
-  .controller('MessagesCtrl', function ($scope, $http, socket) {
+  .controller('MessagesCtrl', function ($scope, $http, socket, Auth, User) {
     $scope.userMessages = [];
+    $scope.user = Auth.getCurrentUser();
 
     $http.get('/api/messages').success(function(messages) {
       $scope.userMessages = messages;
-      socket.syncUpdates('Message', $scope.awesomeThings);
+      socket.syncUpdates('Message', $scope.userMessages);
     });
 
     $scope.addMessage = function() {
       if($scope.newMessage === '') {
         return;
       }
-      $http.post('/api/messages', { name: $scope.newMessage });
+
+      $http.post('/api/messages', {  user: $scope.user.name, name: $scope.newMessage });
       $scope.newMessage = '';
     };
 
